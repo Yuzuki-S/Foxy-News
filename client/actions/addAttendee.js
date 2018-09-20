f392431bfdfde00b539292f481dc04f23a3571c4
leslie-alldridge <leslie.alldridge@gmail.com>
2018-09-20 21:45:22 +1200

    lukelez

--- client/actions/addAttendee.js
+++ client/actions/addAttendee.js
@@ -1,6 +1,34 @@
-export const addAttendee = (user) => {
+import request from "../utils/api";
+
+export const addAttendee = user => {
   return {
-    type: 'ADD_USER',
+    type: "ADD_USER",
     user: user
-  }
+  };
+};
+
+export function getMeeting(id) {
+  return dispatch => {
+    // We dispatch requestLogin to kickoff the call to the API
+
+    return request("get", "api/meetings/1/users", id)
+      .then(response => {
+        if (!response.ok) {
+          console.log("broken");
+
+          // If there was a problem, we want to
+          // dispatch the error condition
+          //dispatch(loginError(response.body.message))
+          // return Promise.reject(response.body.message)
+        } else {
+          // If login was successful, set the token in local storage
+          //const userInfo = saveUserToken(response.body.token)
+          // Dispatch the success action
+          console.log(response);
+
+          //dispatch(receiveLogin(userInfo))
+        }
+      })
+      .catch(err => dispatch(loginError(err.response.body.message)));
+  };
 }
--- client/reducers/attendee.js
+++ client/reducers/attendee.js
@@ -1,13 +1,16 @@
-import {ADD_USER} from '../actions/addAttendee'
-
 const addAttendee = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_USER':
-    console.log(action)
-    return [...state, action]
-    default:
-    return state 
-  }
-}
+    case "ADD_USER":
+      console.log(action);
+      return [...state, action];
 
-export default addAttendee
+    case "GET_MEETING":
+      console.log("reducer");
+      return [...state, action];
+
+    default:
+      return state;
+  }
+};
+
+export default addAttendee;
