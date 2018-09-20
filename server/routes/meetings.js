5076468aa30218a3c275af82efc5ddd3c6eab6f1
lianna-blanca <lianna.blanca@gmail.com>
2018-09-21 09:08:07 +1200

    Placeholder routes now waiting for interconnectivity tests

--- server/routes/meetings.js
+++ server/routes/meetings.js
@@ -1,69 +1,95 @@
 const router = require("express").Router()
-// const {functionsfromDB} = require('../db/meetings')
-
-// Test content
-const meetings = [
-    {
-      "id" : 1,
-      "meetingName" : "First meeting",
-      "attendees" : [
-        {
-        "userId" : 1234,
-        "userName" : "handle name",
-        "firstName" : "Name",
-        "lastName" : "Name",
-        "personCostPerHour" : 100.00,
-        }
-      ],
-      "time": "timestamp",
-      "meetingCostPerHour" : 250.00,
-      "totalMeetingTimeSeconds" : 360,
-      "totalCostOfMeeting" : 6000.00
-    },
-    {  "id" : 2,
-      "meetingName" : "Second meetings",
-      "attendees" : [
-        {
-        "userId" : 5678,
-        "userName" : "handle name",
-        "firstName" : "Name",
-        "lastName" : "Name",
-        "personCostPerHour" : 100.00,
-        }
-      ],
-      "time": "timestamp",
-      "meetingCostPerHour" : 250.00,
-      "totalMeetingTimeSeconds" : 360,
-      "totalCostOfMeeting" : 6000.00
-    }
-  ]
-
-// // Test send to Postman - working
-// router.get("/", (req, res) => {
-//   res.send(meetings)
-// })
+// const {imported_functions} = require('../db/meetings')
 
 
-// Get a Users Meeting Histroy
+// ------- Get a Users Meeting Histroy
 router.get("/", (req, res) => {
-  db.getMeetings()
-  .then((meetings) => {
-      res.json(meetings)
-  })  
-})
-
-// Save a completed meeting
-router.post("/", (req, res) => {
-  // placeholder
-})
-
-// Get the attendees of a Meeting
-router.get("/:id/users", (req, res) => {
-  db.getMeetingsById(req.params.id)
-  .then((meetings) => { 
-//to be id of meeting used to return meeting users?
-      res.json(meetings)
+  db.getUsersMeetings()
+  .then(usersMeetings => {
+    let x = usersMeetings.map(eachMeeting => {
+      return getMeeting(eachMeeting.id)
+    })
+    .then(data => {
+      res.json(data)
+    }
+    )
   })
+  .catch(err => res.status(500)
+    .send({
+      message: "Server Error"
+    }))
 })
+
+// ------- Save a completed meeting
+router.post("/", (req, res) => {
+  saveMeeting()
+  .then(meeting => {
+    res.json(meeting)
+  })
+  .catch(err => res.status(500)
+    .send({
+      message: "Server Error"
+    }))
+})
+
+
+// ------- Get the attendees of a Meeting
+router.get("/:id/users", (req, res) => {
+  db.getAttendees(req.params.id)
+  .then(attendees => {
+    // modify as required
+    res.json(attendees)
+  })
+  .catch(err => res.status(500)
+    .send({
+      message: "Server Error"
+    }))
+})
+
 
 module.exports = router
+
+
+
+
+// // Test content
+// const meetings = [
+//     {
+//       "id" : 1,
+//       "meetingName" : "First meeting",
+//       "attendees" : [
+//         {
+//         "userId" : 1234,
+//         "userName" : "handle name",
+//         "firstName" : "Name",
+//         "lastName" : "Name",
+//         "personCostPerHour" : 100.00,
+//         }
+//       ],
+//       "time": "timestamp",
+//       "meetingCostPerHour" : 250.00,
+//       "totalMeetingTimeSeconds" : 360,
+//       "totalCostOfMeeting" : 6000.00
+//     },
+//     {  "id" : 2,
+//       "meetingName" : "Second meetings",
+//       "attendees" : [
+//         {
+//         "userId" : 5678,
+//         "userName" : "handle name",
+//         "firstName" : "Name",
+//         "lastName" : "Name",
+//         "personCostPerHour" : 100.00,
+//         }
+//       ],
+//       "time": "timestamp",
+//       "meetingCostPerHour" : 250.00,
+//       "totalMeetingTimeSeconds" : 360,
+//       "totalCostOfMeeting" : 6000.00
+//     }
+//   ]
+
+// // // Test send to Postman - working
+// // router.get("/", (req, res) => {
+// //   res.send(meetings)
+// // })
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,58 +1,62 @@
 const router = require("express").Router()
 
-// Test send json - working in Postman
-router.get("/", (req, res) => {
-  res.json({
-  "array" : [
-    {
-      "id" : 1234,
-      "userName" : "handle name",
-      "firstName" : "Name",
-      "lastName" : "Name",
-      "personCostPerHour" : 100.00
-    },
-    {
-      "id" : 5678,
-      "userName" : "handle name",
-      "firstName" : "Name",
-      "lastName" : "Name",
-      "personCostPerHour" : 100.00
-    }
-  ]
-})
-})
-
-// function getUsers() {
-// let arr = 
-//   [
+// // Test send json - working in Postman
+// router.get("/", (req, res) => {
+//   res.json({
+//   "array" : [
 //     {
 //       "id" : 1234,
 //       "userName" : "handle name",
 //       "firstName" : "Name",
 //       "lastName" : "Name",
-//       "personCostPerHour" : 100.00,
+//       "personCostPerHour" : 100.00
 //     },
 //     {
 //       "id" : 5678,
 //       "userName" : "handle name",
 //       "firstName" : "Name",
 //       "lastName" : "Name",
-//       "personCostPerHour" : 100.00,
-//     },
+//       "personCostPerHour" : 100.00
+//     }
 //   ]
-
-// return arr
-// }
-
-// // Get the users of the app
-// router.get("/", (req, res) => {
-//   getUsers()
-//   .then(users => {
-//     const thing = users.map(user => {
-//       return user 
-//     })
-//   })
-//   res.json(thing)
+// })
 // })
 
+
+
+// Get the users of the app
+router.get("/", (req, res) => {
+  getUsers()
+   .then(users => {
+    res.json(users)
+  })
+  // .then(users => {
+  //   users.map(user => {
+  //     return user // modify as needed
+  //   })
+  //   res.json(users)
+  // })
+
+  .catch(err => res.status(500)
+    .send({
+      message: "Server Error"
+    })
+  )
+})
+
+
 module.exports = router
+
+
+
+
+// let { getUsers } = require("../db/users");
+
+// router.get("/", getUsersAPI);
+
+// function getUsersAPI(req, res, next) {
+//   getUsers().then(users => {
+//     console.log(users);
+//     res.json(users);
+//   });
+// }
